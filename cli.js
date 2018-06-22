86b165406ebc19b671db183a749c294c9bb5cdf5
Susumu Seino <seino@susu.mu>
2018-06-22 21:48:35 +0200

    Update option

--- cli.js
+++ cli.js
@@ -11,10 +11,16 @@ program
   .usage('[options] <URL>')
   .option('--user <username>', 'The username for the WordPress account.')
   .option('--password <password>', 'The password for the WordPress account.')
+  .option('--title <title>', 'The title for the Post title .')
+  .option('--content <content>', 'The content for the Post title .')
+  .option('--status <status>', 'The status for the Post title .')
 .parse(process.argv);
 
 const user = program.user;
 const pass = program.password;
+const title = program.title;
+const content = program.content;
+const status = program.status;
 
 const wpconfig = wp.createClient({
 	url: program.args[0],
@@ -23,9 +29,9 @@ const wpconfig = wp.createClient({
 });
 
 wpconfig.newPost({
-	title: "きんたま",
-	content: "うんこもりもり",
-	status: "publish"
-}, (error) => {
+	title: title,
+	content: content,
+	status: status,
+}, (error, data) => {
 	console.log(error);
 });
